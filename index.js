a2ef635dc8ed78f0119589dd1429863ea68e4266
David Marongiu <davidmarongiu@hotmail.com>
2026-04-10 16:21:01 +0200

    route app.get person

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send('Backend SW OK');
 });
 
+app.get('/data/person', async (req, res) => {
+    const response = await fetch('https://swapi.info/api/people');
+    const swPersonData = await response.json();
+
+    res.json(swPersonData)
+});
+
 app.listen(3001, () => {
     console.log('Server running on port 3001');
 });
