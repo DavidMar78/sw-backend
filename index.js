36029cfbfeb3b46bb60b0fc08daa9bb421fc8811
David Marongiu <davidmarongiu@hotmail.com>
2026-04-10 15:55:06 +0200

    modif erreur 2

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/people', async (req, res) => {
-
-    const response = await fetch('https://swapi.info/api/people');
-    const peopleData = await response.json();
-
-    res.json(
-        peopleData
-    )
-
+app.get('/', (req, res) => {
     res.send('Backend SW OK');
 });
 
 app.listen(3001, () => {
-    console.log('Server running on http://192.168.1.59:3001');
+    console.log('Server running on port 3001');
 });
