23bc469697a79d26bf8b3f0c6781ae447bb374c8
David Marongiu <davidmarongiu@hotmail.com>
2026-04-10 15:05:48 +0200

    first real commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/people', async (req, res) => {
+
+    const response = await fetch('https://swapi.info/api/people');
+    const peopleData = await response.json();
+
+    constant
+
+    res.json(
+        peopleData
+    )
+
+    res.send('Backend SW OK');
+});
+
+app.listen(3001, () => {
+    console.log('Server running on http://192.168.1.59:3001');
+    console.log()
+});
